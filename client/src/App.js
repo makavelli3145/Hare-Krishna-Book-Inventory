7d94cac91073ad122c18fe12902418c4652b4a0c
makavelli3145 <casseteinc@gmail.com>
2022-05-02 12:07:16 +0200

    shoppingListApp without redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/AppNavbar';
-
+import BookListItems from './components/BookListItems';
 
 function App() {
   return (
     <div className="App">
       <AppNavbar/>
+      <BookListItems/>
     </div>
   );
 }
---
+++ client/src/components/BookListItems.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import {v1 as uuid} from 'uuid';
+
+class BookListItems extends Component
+{
+state = { 
+items: [
+{ id: uuid(), name: 'The science of self realization' },
+{ id: uuid(), name: 'Bhagavad Gita As it is' },
+{ id: uuid(), name: 'Journey to other planets' },
+{ id: uuid(), name: 'The Mahabarata' }
+]
+}
+
+render() {
+  const {items } = this.state;
+return(
+<Container>
+<Button 
+color="dark" 
+style={ {marginBottom: '2rem' } } 
+onClick = {
+() => { const name = prompt('Enter Item');
+if(name) {
+this.setState(state => ({
+items: [...state.items, {id: uuid(),name}]
+}));
+} 
+}}
+
+>Add Item</Button>
+
+<ListGroup>
+<TransitionGroup className="shopping-list">
+{items.map(({id,name}) => (
+ <CSSTransition key={id} timeout={500} classNames="fade">
+	<ListGroupItem>
+		<Button
+ className="remove-btn"
+ color="danger"
+ size="sm"
+ onClick = {() => {
+this.setState(state => ({
+items: state.items.filter(item => item.id !== id)
+}))
+}}
+
+>&times;</Button>
+		{name}
+	</ListGroupItem>
+ </CSSTransition>
+))}
+</TransitionGroup>
+</ListGroup>
+</Container>
+);
+}
+
+}
+
+export default BookListItems; 
